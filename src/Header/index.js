318452661c62a764a4c69d12c3b45abce0862378
Tass2T <brunog90@gmail.com>
2020-11-07 16:15:20 +0100

    mobile ready interface

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,19 +1,47 @@
-
+import {useState} from 'react'
 import './index.css'
 import {Link} from 'react-router-dom'
 
 export default function Header ({logo}) {
+
+    const [menu, setMenu] = useState(true)
+
+    const displayMenu = () => {
+        setMenu(!menu)
+    }
 
     return (
         <div className="header-component">
             <div className="navbar">
                 <div className="logo"><Link to='/'><img src={logo}/></Link></div>
                 <ul className="nav-links">
-                    <li className='nav-item'><Link to='/'>Home</Link></li>
-                    <li className='nav-item'><Link to='/services'>Services</Link></li>
-                    <li className='nav-item'><Link to='/about'>About</Link></li>
+                <Link to='/'><li className='nav-item'><p className="nav-items-text">Home</p></li></Link>
+                <Link to='/services'><li className='nav-item'><p className="nav-items-text">Services</p></li></Link>
+                <Link to='/about'><li className='nav-item'><p className="nav-items-text">About</p></li></Link>
                     <li><button className="sign-up">SIGN UP</button></li>
                 </ul>
+                <div className="burger-icon">
+                    <i class="fas fa-bars fa-2x" onClick={displayMenu}></i>
+                </div>
+            </div>
+            <div className={menu? "mobile-navbar-open" : "mobile-navbar"}>
+                <div className="transparent" onClick={displayMenu}></div>
+                <div className="menu-nav">
+                    <div className="close-button" onClick={displayMenu}>
+                        <i class="fas fa-times fa-3x"></i>
+                    </div>
+                    <div className="mobile-navbar-content">
+                        <ul>
+                            <Link to='/'><li className='nav-item-mobile'><p className="nav-items-text">Home</p></li></Link>
+                            <hr/>
+                            <Link to='/services'><li className='nav-item-mobile'><p className="nav-items-text">Services</p></li></Link>
+                            <hr/>
+                            <Link to='/about'><li className='nav-item-mobile'><p className="nav-items-text">About</p></li></Link>
+                        </ul>
+                    </div>
+                    <button className="sign-up mobile-button">SIGN UP</button>
+                    
+                </div>
             </div>
         </div>
     )
