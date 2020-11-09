afd37b9d8c3dffd6e5596953a49b02cf5b578190
Tass2T <brunog90@gmail.com>
2020-11-09 09:31:13 +0100

    made minor adjustement on home display for mobile

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,10 +3,13 @@ import './index.css'
 export default function Home () {
 
     return (
-        <div className="home-section">
-                <h1>ADVENTURE AWAITS</h1>
-                <p>What are you waiting for?</p>
-                <button>GET STARTED</button>
-        </div>
+        <>
+            <div className="home-section">
+                    <h1>ADVENTURE AWAITS</h1>
+                    <p>What are you waiting for?</p>
+                    <button>GET STARTED</button>
+            </div>
+            
+        </>
     )
 }
